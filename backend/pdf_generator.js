bd2aceb3b14638dc011f7a5df71806c31981830f
omkar p <p.omkar@starkdigital.net>
2022-08-23 13:56:33 +0530

    added pdf changes to generate pdf

--- backend/pdf_generator.js
+++ backend/pdf_generator.js
@@ -270,7 +270,49 @@ module.exports = function(data, id) {
             align: "center"
         })
         doc.setDrawColor("#fff")
-        doc.addImage($.img("disclaimer", "png"), 15, 50, 180, 165.2)
+        // doc.addImage($.img("disclaimer", "png"), 15, 50, 180, 165.2)
+        normal();
+        doc.setFontSize(10)
+        var strArr = doc.splitTextToSize("This conceptual representation is meant to demonstrate the raw power of interest—whether it is working for you or against you. Care has been taken to ensure the accuracy of the information presented, but even so, because there are several moving parts, certain assumptions had to be made and relied upon in making these calculations. Although the figures in this presentation may be helpful in making decisions, they should not be relied upon in place of your own due diligence.", 150)
+        doc.text(strArr, 24, 50,{
+            align:"justify",maxWidth:160
+        });
+        doc.text("Among the assumptions made are the following:.", 24,76)
+        doc.text('\u2022  '+ "That you will qualify for life insurance under the presented parameters: that your health", 28,83)
+        doc.text("and history are such that you are insurable; that you have enough insurable interest", 31,88)
+        doc.text("(based on, among other things, your age and income) to qualify for the amount of death", 31,93)
+        doc.text("benefit in the plan; and that you have enough income and liquid assets to qualify for the", 31,98)
+        doc.text("amount of premium going into the plan.", 31,103)
+        doc.text('\u2022  '+ "That the insurance company providing coverage will continue to pay dividends at their", 28,110)
+        doc.text("projected rate (the rate projected in the illustration, which is based on the current", 31,115)
+        doc.text("dividend scale).", 31,120)
+        doc.text('\u2022  '+ "That the debt information you provided us is accurate, and the interest rates presented", 28,125)
+        doc.text("will not change. That the principal balance will not increase and will decrease as you", 31,130)
+        doc.text("continue to make your debt payments.", 31,135)
+        doc.text('\u2022  '+ "That you will follow the plan as presented. Although the plan is flexible, and you have", 28,140)
+        doc.text("the freedom to make changes as time passes (and you should as circumstances arise),", 31,145)
+        doc.text("any changes you make to your plan will have an effect on the overall outcome.", 31,150)
+        var assump = doc.splitTextToSize("If some or all of these assumptions prove, over time, not to be true, it could have anywhere from slight to significant changes to your plan. Although we believe this is among the safest places to put your money and certainly the best option we are aware of to pay off debt without sacrificing retirement and other financial plans, no decision, including decisions related to place your money, is without some risk.", 150)
+        doc.text(assump, 24, 156,{
+            align:"justify",maxWidth:161
+        });
+        var rate = doc.splitTextToSize("All rates of return are projected and are by no means guaranteed. Like all financial plans, we can make recommendations based on patterns in the past and signs about what will happen in the future, but we cannot see the future.", 150)
+        doc.text(rate, 24, 178,{
+            align:"justify",maxWidth:161
+        });
+        var report = doc.splitTextToSize("This report should not be considered legal, financial, or accounting advice. This material has been prepared for informational purposes only and is not solicitation or an offer or buy a security or instrument or to participate in any trading/investment strategy.", 150)
+        doc.text(report, 24, 192,{
+            align:"justify",maxWidth:161
+        });
+        var concl = doc.splitTextToSize("The conclusions depicted in the illustrations are based on the specific information you provided, and to the extent anything you provided is imprecise or inaccurate, it will have a profound impact on the numbers presented and could materially impact the value of the illustration for comparative purposes.", 150)
+        doc.text(concl, 24, 206,{
+            align:"justify",maxWidth:161
+        });
+
+        var qualify = doc.splitTextToSize("Not everyone will qualify for an insurance contract. Some will be uninsurable. Others will qualify, but not for the full amount desired. Individual circumstances and qualifications always vary.", 150)
+        doc.text(qualify, 24, 225,{
+            align:"justify",maxWidth:161
+        });
 
         $.pageNumber();
 
@@ -279,24 +321,162 @@ module.exports = function(data, id) {
         doc.setTextColor("#000");
         doc.setFontSize(18);
         doc.setFont("Helvetica", "bold");
-        doc.text("How Does the   G.O.L.D.E.N. Program Work ?", doc.internal.pageSize.getWidth() / 2, 40, {
+        doc.text("How Does the G.O.L.D.E.N. Program Work?", doc.internal.pageSize.getWidth() / 2, 40, {
             align: "center"
         })
-        doc.addImage($.img("working1_1.2", "png"), 15, 50, 180, 165.2);
+        // doc.addImage($.img("working1_1.2", "png"), 15, 50, 180, 165.2);
         // doc.addImage($.img("working 1_2", "png"), 14, 234, 180, 38.25)
+        normal();
+        doc.setFontSize(10)
+        var proudest = doc.splitTextToSize("One of Einstein’s proudest accomplishments was the Rule of 72. With it, Einstein created a formula to determine how long it will take for interest to cause your principal investment, or debt, to double.", 129)
+        doc.text(proudest, 24, 50,{
+            align:"justify",maxWidth:160
+        });
+        var calculation = doc.splitTextToSize("The calculation is simple: divide your interest rate into 72, and the resulting number is the number of years it will take for your investment (or debt) to double.", 132)
+        doc.text(calculation, 24, 65,{
+            align:"justify",maxWidth:160
+        });
 
+        var invest = doc.splitTextToSize("If you invest $100 dollars in a vehicle where you get a 4% annual rate of return (which return is compounded), then the Rule of 72 tells us that you will have $200 (double your investment) in 18 years (72 divided by 4).", 150)
+        doc.text(invest, 24, 75,{
+            align:"justify",maxWidth:160
+        });
+        var invest = doc.splitTextToSize("If you have a $100 debt incurring an interest rate of 18%, then the Rule of 72 tells us that (barring any payments in the meantime), you will owe $200 on that debt in four years (72 divided by 18). Of course, this calculation assumes you aren’t making regular payments toward the debt (and you will be). Nevertheless, it is a helpful tool for illustrating just how powerful interest is.", 150)
+        doc.text(invest, 24, 89,{
+            align:"justify",maxWidth:160
+        });
+        var money = doc.splitTextToSize("Traditionally, we think about money as linear. One-dimensional. We earn it, we hold it for a time (during which time we may invest), then we spend it. Before we spend it, we have to choose how that dollar will provide us the most benefit. Do we meet the urgent needs and buy groceries or fix the roof? Do we think ahead to the future and buy term insurance? Or do we reward ourselves with a vacation? A single dollar can only be used towards one of these things, so we must choose carefully. After all, once we spend it, it has exhausted its usefulness, and we must replenish our store of money through additional earnings. But that’s not how we should be thinking about money.", 150)
+        doc.text(money, 24, 107,{
+            align:"justify",maxWidth:160
+        });
+        var dollar = doc.splitTextToSize("Instead, we should consider a dollar not as part of a timeline, with an effective start and an end, but rather as a mountain spring that produces constantly and forever.", 150)
+        doc.text(dollar, 24, 141,{
+            align:"justify",maxWidth:160
+        });
+        var golden = doc.splitTextToSize("The GOLDEN program is based on two fundamental principles: (1) any dollar you earn can be a dollar that works for you forever, and (2) if interest works for you instead of against you, you will not just be debt free, but wealthy.", 150)
+        doc.text(golden, 24, 151,{
+            align:"justify",maxWidth:160
+        });
+        var debt = doc.splitTextToSize("Other debt programs have the chief (and only) goal of eliminating your debt. You scrimp and save, make limited purchases, and put all your money towards your debt. And if your goal is merely to get out of debt, many of these programs work very well for that. But they do so at the expense of your long-term goals. Indeed, after spending all of your extra money on debt, when you finally free yourself from your creditors, you find yourself proclaiming “Financial Freedom!” and then you realize that although you’re debt free, you’re also broke. And you have now lost your best earning years.", 150)
+        doc.text(debt, 24, 165,{
+            align:"justify",maxWidth:160
+        });
+        var future = doc.splitTextToSize("You don’t have to choose between paying off debt and investing for your future. You can earn money now. Even while you eliminate your debt. And doing it this way, you can get rid of your debt faster, even while building wealth.", 150)
+        doc.text(future, 24, 195,{
+            align:"justify",maxWidth:160
+        });
+        doc.text("Here’s how it works:", 24, 210,{
+            align:"justify",maxWidth:160
+        });
+        var tax = doc.splitTextToSize("First, we set up a special tax-advantaged vehicle called an insurance contract An insurance contract is an investment vehicle with the following attributes:", 150)
+        doc.text(tax, 24, 215,{
+            align:"justify",maxWidth:160
+        });
+        doc.text("\u2022  "+ "It is a liquid fund, much like a savings account, except that instead of growing at 0.1% in interest, it ",28,225,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("grows via guaranteed interest and non-guaranteed dividends at a rate that usually approaches or exceeds 5%.",32,230,{
+            align:"justify",maxWidth:152
+        })
         $.pageNumber()
 
         doc.addPage()
         $.pageNumber()
             // doc.addImage($.img("working 1_2", "png"), 14, 234, 180, 38.25);
-        doc.addImage($.img("working 2_1", "png"), 15, 50, 180, 165.2);
+        // doc.addImage($.img("working 2_1", "png"), 15, 50, 180, 165.2);
+        normal();
+        doc.setFontSize(10)
+        doc.text("\u2022  "+ "If used correctly (we will teach you how), all the growth in the account is tax-free and therefore",31, 50,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("creates a tax-free retirement fund. It’s a lot like a Roth account, but without the contribution",34, 55,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("limits, restrictive investment parameters, and lack of guarantees.",34, 60,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("\u2022  "+ "When used through the GOLDEN program, it becomes a powerful tool for eliminating debt by",31, 65,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("converting interest accruing against you to interest earning in your favor.",34, 70,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("\u2022  "+ "The insurance contract is also a whole-life, permanent life insurance policy, which means it",31, 75,{
+            align:"justify",maxWidth:152
+        })
+        doc.text("comes with a death benefit and living benefits (much like long-term care).",34, 80,{
+            align:"justify",maxWidth:157
+        })
+
+        var insurance = doc.splitTextToSize("Thus, the insurance contract is a savings account and emergency fund (but better because it is growing), a retirement account (but better because your principal is guaranteed against loss, and all funds used for retirement are not taxed), a debt-elimination program (but better because it gets rid of debt faster without having to sacrifice retirement contributions and growth), and a life insurance contract (but better than term because it is guaranteed and doesn’t expire).", 129)
+        doc.text(insurance, 24, 85,{
+            align:"justify",maxWidth:160
+        });
+        var insurance = doc.splitTextToSize("To set up the insurance contract, we have to determine how much you can contribute. We do this by gathering information about your finances and figuring out how much you (1) have in liquid accounts accumulated that can be used to jump start your program and (2) are sending out the door to inefficient and inferior expenditures that we can redirect to a program where money will work for you forever:", 129)
+        doc.text(insurance, 24, 111,{
+            align:"justify",maxWidth:160
+        });
+        doc.text("\u2022  "+ "Do you have a qualified account, like a 401(k), IRA, or SEP? If so, let’s use it. Ours is a retirement",28, 138,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("account, but better. ",31, 143,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("\u2022  "+ "Are you regularly contributing to a savings account or retirement account? If so, let’s redirect that",28, 148,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("money into this superior account.",31, 153,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("\u2022  "+ "Are you overpaying on any of your debts? If so, let’s redirect the overpayments because ours is a",28, 158,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("debt elimination program, but better. Plus, we want that money working for you, not paying debt",31, 163,{
+            align:"justify",maxWidth:157
+        })
+        doc.text("directly, where it will never work for you again.",31, 168,{
+            align:"justify",maxWidth:157
+        })
+        var Armed = doc.splitTextToSize("Armed with that information, we create an IGICinsurance contract illustration. The advantage we have when we draw up an illustration for an IGICinsurance contract is that, unlike qualified plans and other investments, the insurance contract actually gives you a 30+ year projection of how your money will perform. We can combine those figures with information about your debt to show you how to optimize your money.", 129)
+        doc.text(Armed, 24, 175,{
+            align:"justify",maxWidth:160
+        });
+        var project = doc.splitTextToSize("With information about your debt, we can project using standard amortization tables, exactly how long it will take you to repay based on your current payment plan, and exactly how much interest you will end up paying. We provide that to you here for sake of comparison.", 129)
+        doc.text(project, 24, 200,{
+            align:"justify",maxWidth:160
+        });
+        var cash = doc.splitTextToSize("Now we have two sets of information: We know how much each debt is shrinking every month as you make payments towards the debts, and we know how much your cash value is growing every month as you make premium payments. Then we let our program get to work.", 129)
+        doc.text(cash, 24, 218,{
+            align:"justify",maxWidth:160
+        });
+        var repayment = doc.splitTextToSize("We optimize your debts for repayment (and usually it’s going to be the smallest debt first). Once the balance remaining on your debt reaches 80% of your growing cash value, you take out a loan from the insurance carrier for the exact balance of your debt, and you pay it off.", 129)
+        doc.text(repayment, 24, 238,{
+            align:"justify",maxWidth:160
+        });
 
 
         doc.addPage();
         $.pageNumber()
-        doc.addImage($.img("working2_2.3", "png"), 15, 50, 180, 165.2);
+        // doc.addImage($.img("working2_2.3", "png"), 15, 50, 180, 165.2);
         // doc.addImage($.img("working3", "png"), 15, 25, 180, 72.34);
+        normal();
+        doc.setFontSize(10)
+        var company = doc.splitTextToSize("Your cash value hasn’t changed. And the insurance company is still paying you interest and dividends on the entire cash value of your policy. But interest is only growing against you on part of it. So since your growth exceeds the interest against you, we have neutralized the debt interest, and you are actually getting paid to pay off your debt.", 129)
+        doc.text(company, 24, 50,{
+            align:"justify",maxWidth:160
+        });
+        var monthly = doc.splitTextToSize("Now, the monthly payment you were making towards that first debt goes into your program. And now those sums are working for you, forever. Instead of against you during your best earning years.", 129)
+        doc.text(monthly, 24, 73,{
+            align:"justify",maxWidth:160
+        });
+        var shrinking = doc.splitTextToSize("We do the same thing for each successive debt. Once the shrinking balance of the debt matches 80% of the growing balance of your cash value, you take out a loan, pay off the debt completely, and then redirect the monthly payment you were making towards debt so you are now paying yourself.", 129)
+        doc.text(shrinking, 24, 87,{
+            align:"justify",maxWidth:160
+        });
+        var wealth = doc.splitTextToSize("The result? Well, we’ll let your numbers speak for themselves (that’s what is on the next several pages), but you’ll get rid of your debts faster than you would otherwise, and you will have wealth growing for you. You’ll be GOLDEN!", 129)
+        doc.text(wealth, 24, 107,{
+            align:"justify",maxWidth:160
+        });
         // current debt trajectory
         doc.addPage();
         detailHeader()
